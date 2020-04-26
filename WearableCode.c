4e9713a710839e2419412c0c193d1608a4f65a4c
mdwrds14 <48966382+mdwrds14@users.noreply.github.com>
2020-04-26 12:47:04 -0500

    Update WearableCode.c

--- WearableCode.c
+++ WearableCode.c
@@ -17,46 +17,44 @@ const int MPU_addr = 0x68;
 int val;
 int prevVal;
  
-int valax; 
-int valay;
-int valaz;
+int vala[3]; //x,y,z
+
 
 RF24 radio(7, 8); // CE, CSN
-const byte address[6] = "0000A";
+const byte address[6] = "00001";
  
 void setup()
 {
     Wire.write(0x6B);
     Wire.write(0);
     Wire.begin();
-    Serial.begin(38400);
+    Serial.begin(115200);
     Serial.println("Initialize MPU");
     mpu.initialize();
     Serial.println(mpu.testConnection() ? "Connected" : "Connection failed");
     radio.begin();
     radio.openWritingPipe(address);
     radio.setPALevel(RF24_PA_MIN);
-    radio.stopListening();
+    radio.stopListening(); 
+//    radio.setDataRate(RF24_250KBPS);
 }
  
 void loop()
 {
   mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
-  valax = map(ax, -17000, 17000, 0, 255) ;
-  valay = map(ay, -17000, 17000, 0, 255);
-  valaz = map(az, -17000, 17000, 0, 255);
+  vala[0] = map(ax, -17000, 17000, 0, 255);//x
+  vala[1] = map(ay, -17000, 17000, 0, 255);//y
+  vala[2] = map(az, -17000, 17000, 0, 255);//z
   Serial.println("ax: ");
-  Serial.println(valax);
+  Serial.println(vala[0]);
   Serial.println("ay:");
-  Serial.println(valay);
+  Serial.println(vala[1]);
   Serial.println("az: ");
-  Serial.println(valaz);
+  Serial.println(vala[2]);
   Serial.println("");
   Serial.println("");
-  delay(1000);
-
-  radio.write(&valax, sizeof(valax));
-  radio.write(&valay, sizeof(valay));
-  radio.write(&valaz, sizeof(valaz));
+  radio.write(vala,sizeof(vala));// transmit data
+  
   delay(500);
+
 }
